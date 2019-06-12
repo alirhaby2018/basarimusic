356d0c3fe76c864b79e362a7478b494d6a29c8f7
alirhaby2018 <46156691+alirhaby2018@users.noreply.github.com>
2019-06-12 15:43:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("prefix : Yplay",{type: 'WATCHING'})
+   client.user.setActivity("prefix : 2play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
